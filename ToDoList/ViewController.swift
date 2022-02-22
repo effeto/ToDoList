814288f55ed3ad6cb0759f711e65b0bdc4a8c0d3
effeto <effectdopler@gmail.com>
2022-02-22 16:00:16 +0200

    Basic functions like adding task, open task for detailed information

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,19 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     @IBOutlet var tableView: UITableView!
     
     var tasks = [String]()
     
+    func updateTasks() {
+        
+        tasks.removeAll()
+        
+        guard let count = UserDefaults().value(forKey:"count") as? Int else {
+            return
+        }
+        
+        for x in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+                tasks.append(task)
+            }
+        }
+        
+        tableView.reloadData() 
+         
+    }
+    
     @IBAction func didTapAdd() {
         let vc =  storyboard?.instantiateViewController(withIdentifier: "entry") as! EntryViewController
         vc.title = "New Task"
+        vc.update = {
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+        }
         navigationController?.pushViewController(vc, animated: true)
     }
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         self.title  = "Tasks"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        if !UserDefaults().bool(forKey: "setup") {
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+            
+        }
+        
+        updateTasks()
+        
     }
     
 }
@@ -30,8 +67,12 @@ extension ViewController: UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc =  storyboard?.instantiateViewController(withIdentifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.task =  tasks[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
-    
 }
 
 extension ViewController: UITableViewDataSource {
@@ -45,6 +86,9 @@ extension ViewController: UITableViewDataSource {
         var content = cell.defaultContentConfiguration()
         
         content.text = tasks[indexPath.row]
+        cell.contentConfiguration = content
+        
+        
         
         return cell
     }
